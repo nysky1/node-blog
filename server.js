6593a868c1a95182d40cb6ebebf834ee50c1f61a
J. Bishop <jbishop@Js-MacBook-Pro.local>
2018-05-27 06:40:50 -0600

    runServer, closeServer

--- server.js
+++ server.js
@@ -9,9 +9,39 @@ const blogListRouter = require('./blog-router');
 //set properties of packages
 app.use(morgan('common'));
 
-app.use('/blog-posts',blogListRouter);
+app.use('/blog-posts', blogListRouter);
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 3000}`);
-  });
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 3000;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`App is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  })
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('CLosing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    })
+  })
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
    
