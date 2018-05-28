179b141ef06900bbb00d6b2031456844669e83bc
J. Bishop <jbishop@Js-MacBook-Pro.local>
2018-05-28 06:27:34 -0600

    forgot static

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const morgan = require('morgan');
 const app = express();
 
 const blogListRouter = require('./blog-router');
-  
+
 //set properties of packages
 app.use(morgan('common'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
