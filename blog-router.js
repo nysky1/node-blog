f50750d754b7f0da977f4299da4e4f0f74c3bf7e
J. Bishop <jbishop@Js-MacBook-Pro.local>
2018-05-30 19:05:55 -0600

    PUT method

--- blog-router.js
+++ blog-router.js
@@ -68,19 +68,27 @@ router.post('/', (req, res) => {
         });
 });
 
-// router.post('/', jsonParser, (req, res) => {
-//     const requiredFields = ['title', 'content', 'author'];
-//     for (let i = 0; i < requiredFields.length; i++) {
-//         const field = requiredFields[i];
-//         if (!(field in req.body)) {
-//             const message = `Missing \`${field}\` in request body`
-//             console.error(message);
-//             return res.status(400).send(message);
-//         }
-//     };
-//     const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-//     res.status(201).json(item);
-// })
+router.put('/:id', (req,res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id ` +
+            `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).json({ message: message });
+    }
+    const toUpdate = {}
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+          toUpdate[field] = req.body[field];
+        }
+      });
+    BlogPosts
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'BlogApp: Internal Server Error'}));
+});
+
 
 // router.put('/:id', jsonParser, (req, res) => {
 //     const requiredFields = ['title', 'content', 'author'];
